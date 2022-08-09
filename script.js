38b178058021205d123a73621094cfe30448ec42
Leo Torres <leot06173@hotmail.com>
2022-08-09 16:01:58 -0700

    Picture border

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //selecting all required elements
 const filterItem = document.querySelector(".items");
-const filterImg = document.querySelectorAll(".gallery .image");
+const filterImg = document.querySelectorAll(".image-container .image");
 
 window.onload = ()=>{ //after window loaded
   filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
@@ -29,24 +29,9 @@ window.onload = ()=>{ //after window loaded
 
 //fullscreen image preview function
 //selecting all required elements
-const previewBox = document.querySelector(".preview-box"),
+const previewBox = document.querySelector(".popup-image"),
 categoryName = previewBox.querySelector(".title p"),
 previewImg = previewBox.querySelector("img"),
 closeIcon = previewBox.querySelector(".icon"),
 shadow = document.querySelector(".shadow");
 
-function preview(element){
-  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
-  document.querySelector("body").style.overflow = "hidden";
-  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
-  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
-  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
-  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
-  previewBox.classList.add("show"); //show the preview image box
-  shadow.classList.add("show"); //show the light grey background
-  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
-    previewBox.classList.remove("show"); //hide the preview box
-    shadow.classList.remove("show"); //hide the light grey background
-    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
-  }
-}
